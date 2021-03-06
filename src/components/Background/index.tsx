8e1680f8d03a7ee6b3898504fc15138333df6fd9
Yovany Cunha <yovany33@gmail.com>
2021-03-06 12:15:52 -0300

    correção no background

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Background = styled.div`
     background-color: ${({theme})=> theme.colors.mainBg};
     width: 100vw;
-    height: 100vh;
+    height: 100%;
 
 `;
 
